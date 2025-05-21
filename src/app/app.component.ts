afdb5e0b4be45b523e5d32ea91e36ec343e0d238
ancqit <ancqit@gmail.com>
2025-05-21 06:11:42 +0530

    updated to 19 and tested primeNG dock

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BaseComponent } from './home/base/base.component';
+import { MessageService } from 'primeng/api';
+import { TerminalService } from 'primeng/terminal';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [BaseComponent],
+  providers:[MessageService,TerminalService],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'lTwist';
---
+++ src/app/home/base/base.component.ts
@@ -0,0 +1,538 @@
+import { Component } from '@angular/core';
+import { DockModule } from 'primeng/dock';
+import { MenuItem, MessageService } from 'primeng/api';
+import { TerminalService } from 'primeng/terminal';
+import { Subscription } from 'rxjs';
+import { AutoFocusModule } from 'primeng/autofocus';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { OverlayBadgeModule } from 'primeng/overlaybadge';
+import { TabsModule } from 'primeng/tabs';
+import { AvatarModule } from 'primeng/avatar';
+import { AvatarGroupModule } from 'primeng/avatargroup';
+import { AnimateOnScrollModule } from 'primeng/animateonscroll';
+import { AccordionModule } from 'primeng/accordion';
+import { AutoCompleteModule } from 'primeng/autocomplete';
+import { BadgeModule } from 'primeng/badge';
+import { BreadcrumbModule } from 'primeng/breadcrumb';
+import { BlockUIModule } from 'primeng/blockui';
+import { ButtonModule } from 'primeng/button';
+import { CalendarModule } from 'primeng/calendar';
+import { DatePickerModule } from 'primeng/datepicker';
+import { CarouselModule } from 'primeng/carousel';
+import { CascadeSelectModule } from 'primeng/cascadeselect';
+import { ChartModule } from 'primeng/chart';
+import { CheckboxModule } from 'primeng/checkbox';
+import { ChipModule } from 'primeng/chip';
+import { ColorPickerModule } from 'primeng/colorpicker';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { ConfirmPopupModule } from 'primeng/confirmpopup';
+import { ContextMenuModule } from 'primeng/contextmenu';
+import { DataViewModule } from 'primeng/dataview';
+import { DialogModule } from 'primeng/dialog';
+import { DividerModule } from 'primeng/divider';
+import { DeferModule } from 'primeng/defer';
+
+import { DragDropModule } from 'primeng/dragdrop';
+import { SelectModule } from 'primeng/select';
+import { DynamicDialogModule } from 'primeng/dynamicdialog';
+import { EditorModule } from 'primeng/editor';
+import { FieldsetModule } from 'primeng/fieldset';
+import { FileUploadModule } from 'primeng/fileupload';
+import { FocusTrapModule } from 'primeng/focustrap';
+import { GalleriaModule } from 'primeng/galleria';
+import { IftaLabelModule } from 'primeng/iftalabel';
+import { InplaceModule } from 'primeng/inplace';
+import { InputMaskModule } from 'primeng/inputmask';
+import { InputSwitchModule } from 'primeng/inputswitch';
+import { InputTextModule } from 'primeng/inputtext';
+import { TextareaModule } from 'primeng/textarea';
+import { InputNumberModule } from 'primeng/inputnumber';
+import { InputGroupModule } from 'primeng/inputgroup';
+import { InputGroupAddonModule } from 'primeng/inputgroupaddon';
+import { InputOtpModule } from 'primeng/inputotp';
+import { ImageModule } from 'primeng/image';
+import { ImageCompareModule } from 'primeng/imagecompare';
+import { KnobModule } from 'primeng/knob';
+import { ListboxModule } from 'primeng/listbox';
+import { MegaMenuModule } from 'primeng/megamenu';
+import { MenuModule } from 'primeng/menu';
+import { MenubarModule } from 'primeng/menubar';
+import { MessageModule } from 'primeng/message';
+import { MessagesModule } from 'primeng/messages';
+import { MultiSelectModule } from 'primeng/multiselect';
+import { MeterGroupModule } from 'primeng/metergroup';
+import { OrganizationChartModule } from 'primeng/organizationchart';
+import { OrderListModule } from 'primeng/orderlist';
+import { OverlayPanelModule } from 'primeng/overlaypanel';
+import { PaginatorModule } from 'primeng/paginator';
+import { PanelModule } from 'primeng/panel';
+import { PanelMenuModule } from 'primeng/panelmenu';
+import { PasswordModule } from 'primeng/password';
+import { PickListModule } from 'primeng/picklist';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
+import { ProgressBarModule } from 'primeng/progressbar';
+import { RadioButtonModule } from 'primeng/radiobutton';
+import { RatingModule } from 'primeng/rating';
+import { SelectButtonModule } from 'primeng/selectbutton';
+import { SidebarModule } from 'primeng/sidebar';
+import { ScrollerModule } from 'primeng/scroller';
+import { ScrollPanelModule } from 'primeng/scrollpanel';
+import { ScrollTopModule } from 'primeng/scrolltop';
+import { SkeletonModule } from 'primeng/skeleton';
+import { SliderModule } from 'primeng/slider';
+import { SpeedDialModule } from 'primeng/speeddial';
+import { SplitterModule } from 'primeng/splitter';
+import { StepperModule } from 'primeng/stepper';
+import { SplitButtonModule } from 'primeng/splitbutton';
+import { StepsModule } from 'primeng/steps';
+import { TableModule } from 'primeng/table';
+import { TabMenuModule } from 'primeng/tabmenu';
+import { TagModule } from 'primeng/tag';
+import { TerminalModule } from 'primeng/terminal';
+import { TieredMenuModule } from 'primeng/tieredmenu';
+import { TimelineModule } from 'primeng/timeline';
+import { ToastModule } from 'primeng/toast';
+import { ToggleButtonModule } from 'primeng/togglebutton';
+import { ToggleSwitchModule } from 'primeng/toggleswitch';
+import { ToolbarModule } from 'primeng/toolbar';
+import { TooltipModule } from 'primeng/tooltip';
+import { TreeModule } from 'primeng/tree';
+import { TreeSelectModule } from 'primeng/treeselect';
+import { TreeTableModule } from 'primeng/treetable';
+import { CardModule } from 'primeng/card';
+import { RippleModule } from 'primeng/ripple';
+import { StyleClassModule } from 'primeng/styleclass';
+import { FloatLabelModule } from 'primeng/floatlabel';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
+import { DrawerModule } from 'primeng/drawer';
+
+
+@Component({
+  selector: 'app-base',
+  imports: [
+    DockModule,
+    AvatarModule,
+    AvatarGroupModule,
+    AnimateOnScrollModule,
+    TabsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AccordionModule,
+    AutoCompleteModule,
+    BadgeModule,
+    BreadcrumbModule,
+    BlockUIModule,
+    ButtonModule,
+    CalendarModule,
+    DatePickerModule,
+    CarouselModule,
+    CascadeSelectModule,
+    ChartModule,
+    CheckboxModule,
+    ChipModule,
+    ColorPickerModule,
+    ConfirmDialogModule,
+    ConfirmPopupModule,
+    ContextMenuModule,
+    DataViewModule,
+    DialogModule,
+    DividerModule,
+    DrawerModule,
+    DeferModule,
+    DragDropModule,
+    SelectModule,
+    DynamicDialogModule,
+    EditorModule,
+    FieldsetModule,
+    FileUploadModule,
+    FocusTrapModule,
+    GalleriaModule,
+    IftaLabelModule,
+    InplaceModule,
+    InputMaskModule,
+    InputSwitchModule,
+    InputTextModule,
+    TextareaModule,
+    InputNumberModule,
+    InputGroupModule,
+    InputGroupAddonModule,
+    InputOtpModule,
+    ImageModule,
+    ImageCompareModule,
+    KnobModule,
+    ListboxModule,
+    MegaMenuModule,
+    MenuModule,
+    MenubarModule,
+    MessageModule,
+    MessagesModule,
+    MultiSelectModule,
+    MeterGroupModule,
+    OrganizationChartModule,
+    OrderListModule,
+    OverlayPanelModule,
+    PaginatorModule,
+    PanelModule,
+    PanelMenuModule,
+    PasswordModule,
+    PickListModule,
+    ProgressSpinnerModule,
+    ProgressBarModule,
+    RadioButtonModule,
+    RatingModule,
+    SelectButtonModule,
+    SidebarModule,
+    ScrollerModule,
+    ScrollPanelModule,
+    ScrollTopModule,
+    SkeletonModule,
+    SliderModule,
+    SpeedDialModule,
+    SplitterModule,
+    StepperModule,
+    SplitButtonModule,
+    StepsModule,
+    TableModule,
+    TabMenuModule,
+    TagModule,
+    TerminalModule,
+    TieredMenuModule,
+    TimelineModule,
+    ToastModule,
+    ToggleButtonModule,
+    ToggleSwitchModule,
+    ToolbarModule,
+    TooltipModule,
+    TreeModule,
+    TreeSelectModule,
+    TreeTableModule,
+    CardModule,
+    RippleModule,
+    StyleClassModule,
+    FloatLabelModule,
+    IconFieldModule,
+    InputIconModule,
+    AutoFocusModule,
+    OverlayBadgeModule,
+  ],
+  standalone: true,
+  templateUrl: './base.component.html',
+  styleUrl: './base.component.scss',
+})
+export class BaseComponent {
+  displayTerminal!: boolean ;
+
+  displayFinder!: boolean ;
+
+  displayGalleria!: boolean;
+
+  dockItems: MenuItem[] | undefined;
+
+  menubarItems: any[] | undefined;
+
+  responsiveOptions: any[] | undefined;
+
+  images: any[] | undefined;
+
+  nodes: any[] | undefined;
+
+  subscription: Subscription | undefined;
+
+  constructor(
+    private messageService: MessageService,
+    private terminalService: TerminalService
+  ) {}
+
+  ngOnInit() {
+    this.dockItems = [
+      {
+        label: 'Finder',
+        tooltipOptions: {
+          tooltipLabel: 'Finder',
+          tooltipPosition: 'top',
+          positionTop: -15,
+          positionLeft: 15,
+          showDelay: 1000,
+        },
+        icon: 'https://primefaces.org/cdn/primeng/images/dock/finder.svg',
+        command: () => {
+          this.displayFinder = true;
+        },
+      },
+      {
+        label: 'Terminal',
+        tooltipOptions: {
+          tooltipLabel: 'Terminal',
+          tooltipPosition: 'top',
+          positionTop: -15,
+          positionLeft: 15,
+          showDelay: 1000,
+        },
+        icon: 'https://primefaces.org/cdn/primeng/images/dock/terminal.svg',
+        command: () => {
+          this.displayTerminal = true;
+        },
+      },
+      {
+        label: 'App Store',
+        tooltipOptions: {
+          tooltipLabel: 'App Store',
+          tooltipPosition: 'top',
+          positionTop: -15,
+          positionLeft: 15,
+          showDelay: 1000,
+        },
+        icon: 'https://primefaces.org/cdn/primeng/images/dock/appstore.svg',
+        command: () => {
+          this.messageService.add({
+            severity: 'error',
+            summary: 'An unexpected error occurred while signing in.',
+            detail: 'UNTRUSTED_CERT_TITLE',
+            key: 'tc',
+          });
+        },
+      },
+      {
+        label: 'Safari',
+        tooltipOptions: {
+          tooltipLabel: 'Safari',
+          tooltipPosition: 'top',
+          positionTop: -15,
+          positionLeft: 15,
+          showDelay: 1000,
+        },
+        icon: 'https://primefaces.org/cdn/primeng/images/dock/safari.svg',
+        command: () => {
+          this.messageService.add({
+            severity: 'warn',
+            summary: 'Safari has stopped working',
+            key: 'tc',
+          });
+        },
+      },
+      {
+        label: 'Photos',
+        tooltipOptions: {
+          tooltipLabel: 'Photos',
+          tooltipPosition: 'top',
+          positionTop: -15,
+          positionLeft: 15,
+          showDelay: 1000,
+        },
+        icon: 'https://primefaces.org/cdn/primeng/images/dock/photos.svg',
+        command: () => {
+          this.displayGalleria = true;
+        },
+      },
+      {
+        label: 'GitHub',
+        tooltipOptions: {
+          tooltipLabel: 'GitHub',
+          tooltipPosition: 'top',
+          positionTop: -15,
+          positionLeft: 15,
+          showDelay: 1000,
+        },
+        icon: 'https://primefaces.org/cdn/primeng/images/dock/github.svg',
+      },
+      {
+        label: 'Trash',
+        tooltipOptions: {
+          tooltipLabel: 'Trash',
+          tooltipPosition: 'top',
+          positionTop: -15,
+          positionLeft: 15,
+          showDelay: 1000,
+        },
+        icon: 'https://primefaces.org/cdn/primeng/images/dock/trash.png',
+        command: () => {
+          this.messageService.add({
+            severity: 'info',
+            summary: 'Empty Trash',
+            key: 'tc',
+          });
+        },
+      },
+    ];
+
+    this.menubarItems = [
+      {
+        label: 'Finder',
+        styleClass: 'menubar-root',
+      },
+      {
+        label: 'File',
+        items: [
+          {
+            label: 'New',
+            icon: 'pi pi-fw pi-plus',
+            items: [
+              {
+                label: 'Bookmark',
+                icon: 'pi pi-fw pi-bookmark',
+              },
+              {
+                label: 'Video',
+                icon: 'pi pi-fw pi-video',
+              },
+            ],
+          },
+          {
+            label: 'Delete',
+            icon: 'pi pi-fw pi-trash',
+          },
+          {
+            separator: true,
+          },
+          {
+            label: 'Export',
+            icon: 'pi pi-fw pi-external-link',
+          },
+        ],
+      },
+      {
+        label: 'Edit',
+        items: [
+          {
+            label: 'Left',
+            icon: 'pi pi-fw pi-align-left',
+          },
+          {
+            label: 'Right',
+            icon: 'pi pi-fw pi-align-right',
+          },
+          {
+            label: 'Center',
+            icon: 'pi pi-fw pi-align-center',
+          },
+          {
+            label: 'Justify',
+            icon: 'pi pi-fw pi-align-justify',
+          },
+        ],
+      },
+      {
+        label: 'Users',
+        items: [
+          {
+            label: 'New',
+            icon: 'pi pi-fw pi-user-plus',
+          },
+          {
+            label: 'Delete',
+            icon: 'pi pi-fw pi-user-minus',
+          },
+          {
+            label: 'Search',
+            icon: 'pi pi-fw pi-users',
+            items: [
+              {
+                label: 'Filter',
+                icon: 'pi pi-fw pi-filter',
+                items: [
+                  {
+                    label: 'Print',
+                    icon: 'pi pi-fw pi-print',
+                  },
+                ],
+              },
+              {
+                icon: 'pi pi-fw pi-bars',
+                label: 'List',
+              },
+            ],
+          },
+        ],
+      },
+      {
+        label: 'Events',
+        items: [
+          {
+            label: 'Edit',
+            icon: 'pi pi-fw pi-pencil',
+            items: [
+              {
+                label: 'Save',
+                icon: 'pi pi-fw pi-calendar-plus',
+              },
+              {
+                label: 'Delete',
+                icon: 'pi pi-fw pi-calendar-minus',
+              },
+            ],
+          },
+          {
+            label: 'Archieve',
+            icon: 'pi pi-fw pi-calendar-times',
+            items: [
+              {
+                label: 'Remove',
+                icon: 'pi pi-fw pi-calendar-minus',
+              },
+            ],
+          },
+        ],
+      },
+      {
+        label: 'Quit',
+      },
+    ];
+
+    this.responsiveOptions = [
+      {
+        breakpoint: '1024px',
+        numVisible: 3,
+      },
+      {
+        breakpoint: '768px',
+        numVisible: 2,
+      },
+      {
+        breakpoint: '560px',
+        numVisible: 1,
+      },
+    ];
+
+    this.subscription = this.terminalService.commandHandler.subscribe(
+      (command: any) => this.commandHandler(command)
+    );
+
+   
+  }
+
+  commandHandler(text: any) {
+    let response;
+    let argsIndex = text.indexOf(' ');
+    let command = argsIndex !== -1 ? text.substring(0, argsIndex) : text;
+
+    switch (command) {
+      case 'date':
+        response = 'Today is ' + new Date().toDateString();
+        break;
+
+      case 'greet':
+        response = 'Hola ' + text.substring(argsIndex + 1) + '!';
+        break;
+
+      case 'random':
+        response = Math.floor(Math.random() * 100);
+        break;
+
+      default:
+        response = 'Unknown command: ' + command;
+        break;
+    }
+
+    if (response) {
+      this.terminalService.sendResponse(response as string);
+    }
+  }
+
+  ngOnDestroy() {
+    if (this.subscription) {
+      this.subscription.unsubscribe();
+    }
+  }
+}
